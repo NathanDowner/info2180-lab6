60e0aab1f9fb81910939c104a605f9e32e17651e
Nathan Downer <nathan.downer@mymona.uwi.edu>
2018-11-13 01:24:54 +0000

    response returns xml document

--- request.php
+++ request.php
@@ -16,11 +16,42 @@ $definition = [
 ];
 
 if ($query === "" and $_GET['all'] == true) {
-    foreach($definition as $d => $def) {
-        print "<h3>". strtoupper($d) ."</h3>";
-        print "<p>". $def ."</p>";
-    }
+    // foreach($definition as $d => $def) {
+    //     print "<h3>". strtoupper($d) ."</h3>";
+    //     print "<p>". $def ."</p>";
+    $xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+    <entries>
+        <definition name="definition" author="John">
+            A statement of the exact meaning of a word, especially in a dictionary.
+        </definition>
+        <definition name="bar" author="John">
+           A place that sells alcholic beverages
+        </definition>
+        <definition name="ajax" author="John">
+           Technique which involves the use of javascript and xml (or JSON)
+        </definition>
+        <definition name="html" author="John">
+           The standard markup language for creating web pages and web applications.
+        </definition>
+        <definition name="css" author="John">
+           A style sheet language used for describing the presentation of a document written in a markup language.
+        </definition>
+        <definition name="javascript" author="John">
+           A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+        </definition>
+        <definition name="php" author="John">
+           A server-side scripting language, and a powerful tool for making dynamic and interactive websites
+        </definition>
+    </entries>';
+    header('Content-Type: text/xml');
+    $xmlOutput = new SimpleXMLElement($xmldata);
+    echo $xmlOutput->asXML();
+    
 } else {
     print "<h3>" . strtoupper($query) . "</h3>";
     print "<p>" . $definition[$query] . "</p>";
 }
+
+
+
+
